b84f6e4adf84f3ca01e792fde3bbe30cb3b8a8fb
lambdalemon <lemonjameslem@protonmail.com>
2025-11-18 00:24:52 -0500

    Adding movement systems

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,30 @@
 
 extern crate pretty_env_logger;
 
-use crate::ecs::Manager;
+use crate::ecs::ordering::{Ordering, SystemOrder};
+use crate::ecs::{EventSystem, Manager, WinitEventSystem};
+use crate::engine::engine_main;
 use crate::logging::setup_logging;
+use crate::systems::movement::{MovementEvent, get_movement, handle_movement};
 use anyhow::Result;
 
 mod ecs;
 mod engine;
 mod init;
 mod logging;
+mod systems;
 
 fn main() -> Result<()> {
     setup_logging();
 
-    let mut manager = Manager::new()?;
-
-    manager.world.add_component(10);
+    let manager = Manager::new()?
+        .add_winit_event_systems(
+            SystemOrder::<WinitEventSystem>::new(engine_main).after(get_movement),
+        )
+        .add_event_handler(
+            MovementEvent::Moved,
+            SystemOrder::<EventSystem>::new(handle_movement),
+        );
 
     manager.run()?;
 
---
+++ src/systems/movement.rs
@@ -0,0 +1,84 @@
+use anyhow::Result;
+use log::*;
+use winit::{
+    event::{Event, KeyEvent, WindowEvent},
+    event_loop::EventLoopWindowTarget,
+    keyboard::{KeyCode, PhysicalKey},
+};
+
+use crate::ecs::{
+    WinitEventSystem, World,
+    events::{EcsEvent, EcsEventData, LemgineEventData},
+    ordering::SystemOrder,
+};
+
+#[derive(Clone, Hash, Eq, PartialEq)]
+pub enum MovementEvent {
+    Moved,
+}
+
+impl EcsEvent for MovementEvent {}
+
+#[derive(Clone)]
+pub struct MovementData {
+    up: bool,
+    down: bool,
+    left: bool,
+    right: bool,
+}
+
+impl EcsEventData for MovementData {}
+
+pub fn handle_movement(world: &World, event_data: LemgineEventData) -> Result<()> {
+    info!("Got some movement here!");
+
+    Ok(())
+}
+
+pub fn get_movement(world: &World, event: Event<()>, _: &EventLoopWindowTarget<()>) -> Result<()> {
+    let mut movement_data = MovementData {
+        up: false,
+        down: false,
+        left: false,
+        right: false,
+    };
+    let mut changed = false;
+
+    if let Event::WindowEvent {
+        window_id: _,
+        event,
+    } = event
+        && let WindowEvent::KeyboardInput {
+            device_id: _,
+            event,
+            is_synthetic: _,
+        } = event
+        && let PhysicalKey::Code(code) = event.physical_key
+    {
+        match code {
+            KeyCode::KeyA => {
+                movement_data.left = true;
+                changed = true;
+            }
+            KeyCode::KeyD => {
+                movement_data.right = true;
+                changed = true;
+            }
+            KeyCode::KeyW => {
+                movement_data.up = true;
+                changed = true;
+            }
+            KeyCode::KeyS => {
+                movement_data.down = true;
+                changed = true;
+            }
+            _ => {}
+        }
+    }
+
+    if changed {
+        world.raise_event(MovementEvent::Moved, movement_data);
+    }
+
+    Ok(())
+}
