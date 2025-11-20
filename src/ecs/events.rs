3ede3a7b98aa41f24be4dd60eed136e6106eb4c3
lambdalemon <lemonjameslem@protonmail.com>
2025-11-19 20:51:52 -0500

    As any

--- src/ecs/events.rs
+++ src/ecs/events.rs
@@ -1,5 +1,6 @@
 use std::any::Any;
 use std::hash::{Hash, Hasher};
+use std::mem::transmute;
 
 pub type LemgineEvent = Box<dyn EventWrapper>;
 pub type LemgineEventData = Box<dyn EventDataWrapper>;
@@ -7,16 +8,33 @@ pub type LemgineEventData = Box<dyn EventDataWrapper>;
 pub trait EcsEvent {}
 pub trait EcsEventData {}
 
-pub trait EventDataWrapper {
+pub trait EventDataWrapper: Any {
     fn clone_box(&self) -> Box<dyn EventDataWrapper>;
+    fn as_any(&self) -> &dyn Any;
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+}
+
+impl dyn EventDataWrapper {
+    pub fn downcast_ref<T: Any>(&self) -> Option<&T> {
+        self.as_any().downcast_ref::<T>()
+    }
+    pub fn downcast_mut<T: Any>(&mut self) -> Option<&mut T> {
+        self.as_any_mut().downcast_mut::<T>()
+    }
 }
 
 impl<T> EventDataWrapper for T
 where
-    T: EcsEventData + Clone + 'static,
+    T: EcsEventData + Clone + 'static + Any,
 {
     fn clone_box(&self) -> Box<dyn EventDataWrapper> {
         Box::new(self.clone())
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
     }
 }
 
