17799acb922c7b38e0c5da693e1fba47bae26013
lambdalemon <lemonjameslem@protonmail.com>
2025-11-19 20:51:59 -0500

    Delta time

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 
 extern crate pretty_env_logger;
 
+use std::time::Instant;
+
 use crate::ecs::order_up::OrderUp;
 use crate::ecs::ordering::{Ordering, SystemOrder};
-use crate::ecs::{EventSystem, Manager, WinitEventSystem};
+use crate::ecs::{EventSystem, Manager, System, WinitEventSystem, World};
 use crate::engine::engine_main;
 use crate::logging::setup_logging;
-use crate::systems::movement::{MovementEvent, get_movement, handle_movement, movement_partial};
+use crate::systems::movement::movement_partial;
 use anyhow::Result;
 
 mod ecs;
@@ -21,9 +23,31 @@ fn main() -> Result<()> {
 
     let manager = Manager::new()?
         .add_winit_event_systems((engine_main as WinitEventSystem,).order_up())
+        .add_systems((update_delta_time as System,).order_up())
+        .add_resource(DeltaTime(0.0))
+        .add_resource(LastTime(Instant::now()))
         .integrate(movement_partial())?;
 
     manager.run()?;
+
+    Ok(())
+}
+
+pub struct DeltaTime(pub f32);
+pub struct LastTime(Instant);
+
+pub fn update_delta_time(world: &World) -> Result<()> {
+    let now = Instant::now();
+
+    let dt = {
+        let mut t = world.get_resource_mut::<LastTime>();
+        let delta = now - t.0;
+        t.0 = now;
+        delta.as_secs_f32()
+    };
+
+    let mut delta_time = world.get_resource_mut::<DeltaTime>();
+    delta_time.0 = dt;
 
     Ok(())
 }
